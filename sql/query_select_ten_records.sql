e2c1fa84887f0099a48998d03c0e33722a08c497
Ana Carolina dos Reis Silva <107812258+tqi-acarolina@users.noreply.github.com>
2022-11-25 21:52:40 -0300

    Update query_select_ten_records.sql

--- sql/query_select_ten_records.sql
+++ sql/query_select_ten_records.sql
@@ -1,1 +1,9 @@
-select * from "population"."population" limit 10
+select * from "populationdb"."population" limit 10;
+
+select * from "populationdb"."population"
+where state = 'Mato Grosso do Sul'
+limit 10;
+
+select * from "populationdb"."population"
+where state = 'Mato Grosso do Sul'
+limit 10;
