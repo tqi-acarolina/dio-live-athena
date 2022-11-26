1e63fe895df397baac7e7da3473ef5900418f0bc
Ana Carolina dos Reis Silva <107812258+tqi-acarolina@users.noreply.github.com>
2022-11-25 21:54:54 -0300

    Update query _sum_all_population.sql

--- sql/query _sum_all_population.sql
+++ sql/query _sum_all_population.sql
@@ -1,3 +1,13 @@
 select
 	sum(population)
-from "population"."population";
+from "populationdb"."population";
+
+select
+    count(distinct city)
+from "populationdb"."population";
+
+select 
+    state, count(distinct city) as qtd_city 
+from "populationdb"."population" 
+group by 1
+limit 10; 
