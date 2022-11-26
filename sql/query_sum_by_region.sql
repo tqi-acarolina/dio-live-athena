756b7be1bfbde30deae6d17ec5d04bcdef584d90
Ana Carolina dos Reis Silva <107812258+tqi-acarolina@users.noreply.github.com>
2022-11-25 21:56:34 -0300

    Update query_sum_by_region.sql

--- sql/query_sum_by_region.sql
+++ sql/query_sum_by_region.sql
@@ -1,5 +1,10 @@
 select region,
 	sum(population)
-from "population"."population"
+from "populationdb"."population"
 where region='Sul'
 group by region;
+
+select region, sum(population)
+from "populationdb"."population"
+group by 1
+having sum(population) > 100000;
