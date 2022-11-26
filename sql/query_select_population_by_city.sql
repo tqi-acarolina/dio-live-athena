c434470d071df3a54d10702a214473f8eeaf898f
Ana Carolina dos Reis Silva <107812258+tqi-acarolina@users.noreply.github.com>
2022-11-25 21:50:45 -0300

    Update query_select_population_by_city.sql

--- sql/query_select_population_by_city.sql
+++ sql/query_select_population_by_city.sql
@@ -1,3 +1,13 @@
 select city, population
-from "population"."population"
+from "populationdb"."population"
 where city='Foz do Iguaçu';
+
+select city, population
+from "populationdb"."population"
+where city='Uberlândia';
+
+select city, population
+from "populationdb"."population"
+where city in ('Foz do Iguaçu', 'São Paulo');
+
+
